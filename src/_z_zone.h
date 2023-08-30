f9bc970ccf25ec1a4d57d9b1174a90da7d78db54
Emile Belanger <emile.belanger@gmail.com>
2023-08-30 20:39:41 +0100

    Fix large memory usage

--- src/_z_zone.h
+++ src/_z_zone.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #define MINFRAGMENT     64
 #define DPMI_INT  0x31
-#define MAXMEMORYSIZE   900000000
+#define MAXMEMORYSIZE   9000000
 
 #define LEVELZONESIZE   250000
 
